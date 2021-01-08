333e97ed1db7ee1737c87956fe71301dd64302ba
Rudywasnthere <rudybcchs@gmail.com>
2021-01-08 05:40:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import math,sys
+
+base_values = {"1":1, "2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"a":10,"b":11,"c":12,"d":13,"e":14,"f":15,"g":16,"h":17,"i":18,"j":19,"k":20,"l":21,"m":22,"n":23,"o":24,"p":25,"q":26, "r":27,"s":28,"t":29,"u":30,"v":31,"w":32,"x":33,"y":34,"z":35}
+
+def main():
+  base_1 = input("Hello, I convert between number-systems \nfor you. Because of my programmers limited \nmental capactiy I only go upto base 36 \n(Covering 0-9 and the English standar alphabet)\nWhat's your starting base:\t")
+  base_1 = correct_inputs(base_1)
+  number_1 = input("Starting number:\t")
+  number_1 = correct_inputs(number_1, number)
+  base_2 = input("Second base to translate into:\t")
+  base_2 = correct_inputs(base_2)
+
+
+def correct_inputs(start_input, type = "base"):
+  main_count = 0
+  count = 0
+  if type == "base":
+    while count < 1:
+      if main_count > 0:
+        start_input = input("I need a correct value:\t")
+      try:
+        useless_1 = base_values[start_input]
+        count += 1
+      except KeyError:
+        count = 0
+      main_count += 1
+  elif base == "number":
+    
+
+def math_time(base_1, base_2, starting_number):
+
+
+main()
